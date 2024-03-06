4abe63f2cc5514d515f5bbe7d8417d0e4bb3a059
prog-ops <akunbaru14102019@gmail.com>
2024-03-06 18:11:29 +0700

    Models

---
+++ app/src/main/java/com/programmer270487/loginsignuptypicode/data/Photo.kt
@@ -0,0 +1,10 @@
+package com.programmer270487.loginsignuptypicode.data
+
+data class Photo(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
+
---
+++ app/src/main/java/com/programmer270487/loginsignuptypicode/data/Role.kt
@@ -0,0 +1,8 @@
+package com.programmer270487.loginsignuptypicode.data
+
+data class Role(
+    val id: String,
+    val username: String,
+    val email: String,
+    val role: Boolean
+)
