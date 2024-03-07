c264e541a26954e15a466faf1da8f69d7fae2521
prog-ops <akunbaru14102019@gmail.com>
2024-03-08 00:10:09 +0700

    Hilt requirement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.isIncrementalKapt
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
     id("com.google.gms.google-services")
     id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -18,12 +22,13 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
     }
 
     buildTypes {
         debug {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -47,6 +52,47 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
+//    packaging {
+////        resources {
+////            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+////        }
+//
+//        resources.excludes.add("META-INF/*")    //solusi junit5 dependensi di android
+//        resources.excludes.add("META-INF/LICENSE")
+//        resources.excludes.add("META-INF/LICENSE.txt")
+//        resources.excludes.add("META-INF/license.txt")
+//        resources.excludes.add("META-INF/NOTICE")
+//        resources.excludes.add("META-INF/NOTICE.txt")
+//        resources.excludes.add("META-INF/notice.txt")
+//        resources.excludes.add("META-INF/ASL2.0")
+//        resources.excludes.add("META-INF/*.kotlin_module")
+//        resources.excludes.add("META-INF/DEPENDENCIES")
+//    }
+//    packaging {
+//        resources {
+//            excludes += "META-INF/INDEX.LIST"
+//            excludes += "META-INF/DEPENDENCIES"
+//        }
+//    }
+    packagingOptions {
+////        resources.excludes.add("META-INF/INDEX.LIST")
+////        resources.excludes.add("META-INF/DEPENDENCIES")
+////        exclude("META-INF/INDEX.LIST")
+////        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/gradle/incremental.annotation.processors")
+//        exclude("META-INF/LICENSE")
+//        exclude("META-INF/LICENSE.txt")
+//        exclude("META-INF/license.txt")
+//        exclude("META-INF/NOTICE")
+//        exclude("META-INF/NOTICE.txt")
+//        exclude("META-INF/notice.txt")
+//        exclude("META-INF/DEPENDENCIES")
+    }
+    kapt {
+        correctErrorTypes = true
+        !isIncrementalKapt()
+    }
 }
 
 dependencies {
@@ -61,11 +107,12 @@ dependencies {
     implementation(libs.retrofitConverterGson)
     implementation(libs.picasso)
     implementation(libs.hilt)
-    implementation(libs.hiltAndroidCompiler)
-    implementation(libs.hiltCompiler)
-    implementation(libs.hiltLifecycle)
-    implementation(libs.picasso)
-    implementation(libs.picasso)
+//    implementation(libs.hiltLifecycle) // don't use since dagger 2.34
+//    implementation(libs.hiltAndroidCompiler)
+//    implementation(libs.hiltCompiler)
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/programmer270487/loginsignuptypicode/ThisApp.kt
@@ -0,0 +1,7 @@
+package com.programmer270487.loginsignuptypicode
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ThisApp: Application()
