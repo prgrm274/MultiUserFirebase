e0aa24ad71530660d0350f9fae802e35ee65a828
prog-ops <akunbaru14102019@gmail.com>
2024-03-08 21:30:28 +0700

    feat: Signup with Hilt

--- app/src/main/java/com/programmer270487/loginsignuptypicode/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/programmer270487/loginsignuptypicode/ui/signup/SignupActivity.kt
@@ -4,28 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.programmer270487.loginsignuptypicode.data.model.Role
 import com.programmer270487.loginsignuptypicode.databinding.ActivitySignupBinding
-import com.programmer270487.loginsignuptypicode.ui.login.LoginViewModel
+import com.programmer270487.loginsignuptypicode.ui.login.AuthViewModel
 import com.programmer270487.loginsignuptypicode.utils.State
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignupActivity : AppCompatActivity() {
     private val b: ActivitySignupBinding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
-
-    private val firebaseAuth = FirebaseAuth.getInstance()
-    private val firestore = FirebaseFirestore.getInstance()
-    private val valid = true
-    val viewModel: LoginViewModel by viewModels()
-//    private val viewModel: LoginViewModel by lazy {
-//        ViewModelProvider(this)[LoginViewModel::class.java]
-//    }
+    val viewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,87 +27,52 @@ class SignupActivity : AppCompatActivity() {
         val getEmail = intent.getStringExtra("email")
         b.emailEditText.setText(getEmail)
 
-//        observe(null)
+        observe()
 
         b.signupAdminButton.setOnClickListener {
-//            signUp(true)
-            observe(true)
+            val uname = b.usernameEditText.text.toString()
+            val email = b.emailEditText.text.toString()
+            val password = b.passwordEditText.text.toString()
+            viewModel.register(
+                uname,
+                email,
+                password,
+                true
+            )
         }
         b.signupButton.setOnClickListener {
-//            signUp(false)
-            observe(false)
+            val uname = b.usernameEditText.text.toString()
+            val email = b.emailEditText.text.toString()
+            val password = b.passwordEditText.text.toString()
+            viewModel.register(
+                uname,
+                email,
+                password,
+                false
+            )
         }
 
         b.textBackToLogin.setOnClickListener {
-//            onBackPressedDispatcher.onBackPressed()
             finish()
         }
     }
 
-    private fun observe(isAdmin: Boolean) {
+    private fun observe() {
         viewModel.register.observe(this) {
             when (it) {
                 is State.Loading -> {
-
+                    b.textPleaseWait.visibility = View.VISIBLE
                 }
                 is State.Success -> {
-                    val uname = b.usernameEditText.text.toString()
-                    val email = b.emailEditText.text.toString()
-                    val password = b.passwordEditText.text.toString()
-                    viewModel.register(
-                        email,
-                        password,
-                        isAdmin
-                    )
-                }
-                is State.Failure -> {}
-            }
-        }
-    }
-
-    private fun signUp(adminRole: Boolean) {
-        val uname = b.usernameEditText.text.toString()
-        val email = b.emailEditText.text.toString()
-        val password = b.passwordEditText.text.toString()
-
-        if (!TextUtils.isEmpty(uname) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
-            // If no error is occured, then register, finish this register screen and back to login screen
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (it.isComplete) {
-                        Toast.makeText(this, "Please wait..", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                .addOnSuccessListener {
-                    Toast.makeText(
-                        this,
-                        "Register successful, please login",
-                        Toast.LENGTH_SHORT
-                    ).show()
-
-                    val user = firebaseAuth.currentUser
-                    val documentReference = firestore.collection("Users").document(user!!.uid)
-                    val userInfo = HashMap<String, Any>()
-                    userInfo["username"] = uname
-                    userInfo["email"] = email
-                    userInfo["adminRole"] = adminRole
-                    documentReference.set(userInfo)
-
-                    val returnIntent = Intent().apply {
-                        putExtra("SIGN UP", email)
-                    }
-                    setResult(RESULT_OK, returnIntent)
+                    b.textPleaseWait.visibility = View.GONE
+                    Toast.makeText(this, it.data, LENGTH_SHORT).show()
                     finish()
                 }
-                .addOnFailureListener {
-                    Toast.makeText(
-                        this,
-                        "Failed register (${it.localizedMessage}",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                is State.Failure -> {
+                    b.textPleaseWait.visibility = View.GONE
+                    Toast.makeText(this, it.error, LENGTH_SHORT).show()
                 }
-        } else {
-            Toast.makeText(this, "Please check all the fields ", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
